d22f0ae9ba8ca770c438b4bcb2e88c3fef356fce
tawhid12 <tawhid99@gmail.com>
2024-04-03 15:12:04 +0600

    correction

--- resources/views/blade_components/homepage-add-section.blade.php
+++ resources/views/blade_components/homepage-add-section.blade.php
@@ -1,6 +1,11 @@
 <section class="section pt-5 pb-5 bg-white homepage-add-section">
         <div class="container">
             <div class="row">
+                <div class="col-md-12 col-12">
+                    <div class="products-box">
+                        <a href=""><img alt="" src="{{asset('')}}assets/img/panta-ilish.jpg" class="img-fluid rounded"></a>
+                    </div>
+                </div>
                 <div class="col-md-3 col-6">
                     <div class="products-box">
                         <a href="listing.html"><img alt="" src="{{asset('')}}assets/img/pro1.jpg" class="img-fluid rounded"></a>
