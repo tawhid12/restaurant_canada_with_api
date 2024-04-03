92dc35aaf712c85147d6336c7396c6ee59539055
tawhid12 <tawhid99@gmail.com>
2024-04-03 15:33:34 +0600

    correction

--- resources/views/festival-reg.blade.php
+++ resources/views/festival-reg.blade.php
@@ -8,5 +8,35 @@
             </div>
         </div>
     </div>
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+          <div class="card">
+            <div class="card-header">
+              Registration Form
+            </div>
+            <div class="card-body">
+              <form>
+                <div class="form-group">
+                  <label for="fullName">Full Name</label>
+                  <input type="text" class="form-control" name="fullName" placeholder="Enter your full name">
+                </div>
+                <div class="form-group">
+                  <label for="mobileNumber">Mobile Number</label>
+                  <input type="tel" class="form-control" name="mobileNumber" placeholder="Enter your mobile number">
+                </div>
+                <div class="form-group">
+                  <label for="email">Email</label>
+                  <input type="email" class="form-control" name="email" placeholder="Enter your email address">
+                </div>
+                <div class="form-group">
+                  <label for="ticketNumber">Ticket Number</label>
+                  <input type="text" class="form-control" name="ticketNumber" placeholder="Enter your ticket number">
+                </div>
+                <button type="submit" class="btn btn-primary">Register</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
 </div>
 @endsection
