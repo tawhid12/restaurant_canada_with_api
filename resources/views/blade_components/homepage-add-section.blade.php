263702011d15be38c1a3edaab7178961d146ed99
tawhid12 <tawhid99@gmail.com>
2024-04-07 10:46:12 +0600

    correction

--- resources/views/blade_components/homepage-add-section.blade.php
+++ resources/views/blade_components/homepage-add-section.blade.php
@@ -1,11 +1,6 @@
 <section class="section pt-5 pb-5 bg-white homepage-add-section">
         <div class="container">
             <div class="row">
-                <div class="col-12">
-                    <div class="products-box d-flex justify-content-center align-items-center my-3">
-                        <a href="{{route('festival_regs.create')}}"><img alt="" src="{{asset('')}}assets/img/panta-ilish.jpg" class="img-fluid rounded"></a>
-                    </div>
-                </div>
                 <div class="col-md-3 col-6">
                     <div class="products-box">
                         <a href="listing.html"><img alt="" src="{{asset('')}}assets/img/pro1.jpg" class="img-fluid rounded"></a>
