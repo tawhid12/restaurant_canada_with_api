5174022c02bfd3a291fc7aec5cd757059477311a
tawhid12 <tawhid99@gmail.com>
2024-04-07 12:37:45 +0600

    correction

---
+++ test.php
@@ -0,0 +1,13 @@
+<?php
+$to = "tawhid8995@gmail.com";
+$subject = "Test Email";
+$message = "This is a test email sent using PHP's mail() function.";
+$headers = "From: info@bdhscanada.com";
+
+// Send email
+if (mail($to, $subject, $message, $headers)) {
+    echo "Email sent successfully.";
+} else {
+    echo "Email sending failed.";
+}
+?>
