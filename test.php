826767c4a827693616a18197238e9b4f7f9fc70f
tawhid12 <tawhid99@gmail.com>
2024-04-08 09:37:24 +0600

    correction

--- test.php
+++ test.php
@@ -2,7 +2,7 @@
 $to = "tawhid8995@gmail.com";
 $subject = "Test Email";
 $message = "This is a test email sent using PHP's mail() function.";
-$headers = "From: info@bdhscanada.com";
+$headers = "From: no-reply@khanapina.bdhscanada.com";
 
 // Send email
 if (mail($to, $subject, $message, $headers)) {
